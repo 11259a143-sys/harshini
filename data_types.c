6dc5dbfb0668e09471889be973aed4a0e9cf5a59
11259a143-sys <11259a143@kanchiuniv.ac.in>
2025-12-02 20:59:27 +0530

    Add files via upload

---
+++ data_types.c
@@ -0,0 +1,12 @@
+
+int main(){
+    int a=10;
+    float b=5.5;
+    char c='A';
+    double d=123.456789;
+    printf("integer:%d\n",a);
+    printf("float:%.2f\n",b);
+    printf("character:%c\n",c);
+    printf("double:%.3lf\n",d);
+    return 0;
+}
